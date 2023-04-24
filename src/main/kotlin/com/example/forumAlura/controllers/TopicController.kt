e92db8fec62e0622e29d0d85ed82d2c2ef1c7398
CodeAkio <siqueira_2004@hotmail.com>
2023-04-24 01:37:48 -0300

    feat: get topic by id

--- src/main/kotlin/com/example/forumAlura/controllers/TopicController.kt
+++ src/main/kotlin/com/example/forumAlura/controllers/TopicController.kt
@@ -1,10 +1,9 @@
 package com.example.forumAlura.controllers
 
-import com.example.forumAlura.models.Course
 import com.example.forumAlura.models.Topic
-import com.example.forumAlura.models.User
 import com.example.forumAlura.services.TopicService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -17,5 +16,10 @@ class TopicController(
     @GetMapping
     fun getAll(): List<Topic> {
         return topicService.getAll()
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): Topic {
+        return topicService.getById(id)
     }
 }
--- src/main/kotlin/com/example/forumAlura/services/TopicService.kt
+++ src/main/kotlin/com/example/forumAlura/services/TopicService.kt
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicService {
-    fun getAll(): List<Topic> {
-        val topic = Topic(
+class TopicService(private var topics: List<Topic>) {
+    init {
+        val topic1 = Topic(
             id = 1,
             title = "Dúvida Kotlin",
             message = "Variável no kotlin",
@@ -25,6 +25,33 @@ class TopicService {
             )
         )
 
-        return Arrays.asList(topic)
+        val topic2 = Topic(
+            id = 2,
+            title = "Dúvida Kotlin",
+            message = "Variável no kotlin",
+            course = Course(
+                id = 1,
+                name = "Kotlin",
+                category = "Programação",
+            ),
+            author = User(
+                id = 1,
+                name = "Pedro",
+                email = "pedro@email.com"
+            )
+        )
+
+        topics = Arrays.asList(topic1, topic2)
+    }
+
+    fun getAll(): List<Topic> {
+        return topics
+    }
+
+    fun getById(id: Long): Topic {
+        return topics.stream()
+            .filter { t -> t.id == id }
+            .findFirst()
+            .get()
     }
 }
